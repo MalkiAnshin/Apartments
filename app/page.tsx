04d4a988380c0aded5cf3e0c0d98076b176fc968
Malki Anshin <120270762+MalkiAnshin@users.noreply.github.com>
2025-04-30 18:09:49 +0300

    TEST 10

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ const HomePage: React.FC = () => {
     <GlobalProvider>
       <div className="container mx-auto p-4">
         <h1 className="text-3xl font-bold mb-6 text-center">
-        %%%% מצא את הנכס או הקרקע המושלם המתאים לצרכים שלך
+         מצא את הנכס או הקרקע המושלם המתאים לצרכים שלך
         </h1>
         <PropertyTypeSelector onSelect={setSelectedType} />
         <div className="mt-6">
