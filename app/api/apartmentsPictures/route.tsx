204b24122670d3a59f5e880ee87b2667d4eaed6f
Malki Anshin <120270762+MalkiAnshin@users.noreply.github.com>
2025-06-15 18:05:12 +0300

    ךל

--- app/api/apartmentsPictures/route.tsx
+++ app/api/apartmentsPictures/route.tsx
@@ -32,6 +32,6 @@ export async function GET(request: Request) {
 
     return NextResponse.json({ images });
   } catch {
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
   }
 }
